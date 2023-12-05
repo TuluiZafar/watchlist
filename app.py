6fb1de1e1b8f555fd129702cd84528cb7dab3a9d
Almare <2021201796@ruc.edu.cn>
2023-12-06 00:03:19 +0800

    Add static files

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask_sqlalchemy import SQLAlchemy # 导入扩展类
+from flask import Flask, render_template
+from flask import url_for
+import os
+import sys
+app = Flask(__name__)
+db = SQLAlchemy(app) # 初始化扩展，传入程序实例 app
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
+WIN = sys.platform.startswith('win')
+if WIN: # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else: # 否则使用四个斜线
+    prefix = 'sqlite:////'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
+
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User: %s' % name
+
+
+@app.route('/test')
+def test_url_for():
+    # 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
+    print(url_for('hello')) # 输出：/
+    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+    print(url_for('user_page', name='greyli')) # 输出：/user/greyli
+    print(url_for('user_page', name='peter')) # 输出：/user/peter
+    print(url_for('test_url_for')) # 输出：/test
+    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL后面。
+    print(url_for('test_url_for', num=2)) # 输出：/test?num=2return 'Test page'
+    return 'Test page'
+
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
